35ab870699b2830f24b56367c5b61519272e1ae6
Karl Ranson <karl_ranson@hotmail.com>
2018-02-17 11:48:32 +0800

    Test

--- Karl/__init__.py
+++ Karl/__init__.py
@@ -1,1 +1,12 @@
 #Efficient_catalog_crossmatching-skymining_hatcahon_2018
+import panda as pd
+from matplotlib import
+scatterplot
+path = 'C:\Users\karl_\Dropbox\Study, business\Data Science, John Hopkins, Coursera\Hackastron\spectra_ADACS'
+os.chdir(path)
+data1 = "172829-28461_spectra_etau"
+csv_file = csv.reader('data1',delimiter=' ')
+
+xs = [x[0] for x in li]
+ys = [x[1] for x in li]
+plt.plot(xs, ys)
